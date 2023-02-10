8e0ffa9e878398eb25e4d0015c3ec9b6e3f0b528
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-09 23:28:37 -0600

    setup server

---
+++ server.ts
@@ -0,0 +1,10 @@
+import app from './app';
+import { connectDatabase } from './services/db';
+async function start() {
+	await connectDatabase();
+	app.listen(process.env.PORT, () => {
+		console.log(`** App listening on port ${process.env.PORT} **`);
+	});
+}
+
+start().catch((e) => console.log(e));
