56821f7e9a43c591d9ce76054ff7bc0b7ad551fc
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-09 23:30:01 -0600

    1. add eslint config 2. add jest config 3. add ts config 4. add body-parse, bcypt, mongoose, ts-node 5. add scripts

---
+++ environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			DATABASE_URL: string;
+			TEST_DATABASE_URL: string;
+			PORT?: string;
+		}
+	}
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
