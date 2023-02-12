8dca46889e764a6a382a1b7b69b1e317d069f5f6
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-11 19:37:00 -0600

    1. add more tests

--- src/controllers/__tests__/session.test.ts
+++ src/controllers/__tests__/session.test.ts
@@ -3,15 +3,9 @@ import app from '../../app';
 import { Session } from '../../models/sessions';
 import { setup, teardown } from '../../setup-teardown';
 import { User } from '../../models/users';
-const user = {
-	email: 'test3@gmail.com',
-	password: 'password',
-	name: 'Test User',
-};
 
 beforeAll(async () => {
 	await setup();
-	await User.create(user);
 });
 
 afterAll(async () => {
@@ -19,6 +13,19 @@ afterAll(async () => {
 });
 
 describe('POST /api/v1/sessions', () => {
+	const user = {
+		email: 'test3@gmail.com',
+		password: 'password',
+		name: 'Test User',
+	};
+	beforeAll(async () => {
+		await User.create(user);
+	});
+
+	afterAll(async () => {
+		await Session.deleteMany({});
+	});
+
 	it('should return 201 with a session token', async () => {
 		const response = await request(app).post('/api/v1/sessions').send({
 			email: user.email,
@@ -31,11 +38,10 @@ describe('POST /api/v1/sessions', () => {
 		const session = await Session.findOne({
 			sessionToken: response.body.sessionToken,
 		});
-		expect(session).toBeTruthy();
 		expect(session?.sessionToken).toBeTruthy();
 	});
 
-	it('should return 401 with an error message', async () => {
+	it('should return 400 with an error message', async () => {
 		const response = await request(app).post('/api/v1/sessions').send({
 			email: user.email,
 			password: 'wrong password',
@@ -43,5 +49,57 @@ describe('POST /api/v1/sessions', () => {
 
 		expect(response.status).toBe(400);
 		expect(response.body.error).toBe('Invalid password');
+	});
+
+	it('should return 401 with an error message', async () => {
+		const response = await request(app).post('/api/v1/sessions').send({
+			email: 'wrong email',
+			password: user.password,
+		});
+
+		expect(response.status).toBe(400);
+		expect(response.body.error).toBe('Invalid email');
+	});
+});
+
+describe('DELETE /api/v1/sessions', () => {
+	const user = {
+		email: 'test4@gmail.com',
+		password: 'password',
+		name: 'Test User',
+	};
+	beforeAll(async () => {
+		await User.create(user);
+	});
+
+	it('response 204', async () => {
+		const signinResponse = await request(app)
+			.post('/api/v1/sessions')
+			.send({
+				email: user.email,
+				password: user.password,
+			});
+
+		const signoutResponse = await request(app)
+			.delete('/api/v1/sessions')
+			.set('Authorization', `Bearer ${signinResponse.body.sessionToken}`);
+
+		expect(signoutResponse.status).toBe(204);
+	});
+
+	it('response 401 with error message', async () => {
+		const response = await request(app).delete('/api/v1/sessions');
+
+		expect(response.status).toBe(401);
+		expect(response.body.error).toBe('Missing authorization header');
+	});
+
+	it('response 401 with error message', async () => {
+		const response = await request(app)
+			.delete('/api/v1/sessions')
+			.set('Authorization', `Bearer wrong token`);
+
+		expect(response.status).toBe(401);
+		expect(response.body.error).toBe('Invalid session token');
 	});
 });
