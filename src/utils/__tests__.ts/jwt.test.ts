bdc413e14a966f75ffb549659fdf6d88d9090db9
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-12 19:14:26 -0600

    remove console.log

--- src/utils/__tests__.ts/jwt.test.ts
+++ src/utils/__tests__.ts/jwt.test.ts
@@ -4,7 +4,6 @@ describe('generateToken', () => {
 	it('should generate a valid token', () => {
 		const expiresIn = 1;
 		const token = generateToken({ id: 1 }, expiresIn);
-		console.log(token);
 		expect(token).toBeTruthy();
 	});
 });
