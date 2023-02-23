1c83b06e7b1e0691b9193a2399aeca554f20a200
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-22 21:55:31 -0600

    add cors and log incoming request and outgoing response

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,36 @@ dotenv.config();
 // Setup app
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(bodyParser.json());
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+		allowedHeaders: ['Authorization', 'Content-Type'],
+	})
+);
 
 // Setup Routing
 import userRoute from './controllers/user';
 import sessionRoute from './controllers/session';
 import trackerRoute from './controllers/tracker';
 import trackerLogRoute from './controllers/trackerLog';
+app.use(function (req, res, next) {
+	// log incoming request
+	console.log(`${req.method} ${req.url}`);
+	// log outgoing response
+	res.on('finish', () => {
+		console.log(
+			`${res.statusCode} ${res.statusMessage}; ${
+				res.get('Content-Length') || 0
+			}b sent`
+		);
+	});
+	next();
+});
+
 app.use('/api/v1/sessions', sessionRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/trackers', trackerRoute);
